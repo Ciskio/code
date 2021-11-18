dfbde3c1d4bcc0bb80c43f9949ee9896ee829559
Francesco <50875832+Ciskio@users.noreply.github.com>
2021-11-18 16:23:25 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -19,23 +19,28 @@ stl.title("Automodeling Software")
 input_file = stl.file_uploader("Drag your input file here")
 
 col1, col2 = stl.columns(2) # make 2 columns for input parameters
-with stl.form('INPUT PARAMETERS'):
-    with col1:
-#parameters = stl.form(key='INPUT PARAMETERS')
-        res = stl.text_input(label='Max acceptable resolution for templates')
-        seqid = stl.text_input(label='Sequence identity threshold')
-        hmm_2_import = stl.text_input(label='HMM to import')
-        pdb_2_use = stl.text_input(label='MAX number of templates to download')
-    with col2:
-        eva = stl.text_input(label='E val')
-        number_of_wt_models = stl.text_input(label='Number of WT model to generate')
-        number_of_mut_models = stl.text_input(label='Number of MUTANTS model to generate')
+# parameters = stl.form(key='INPUT PARAMETERS')
+with stl.form(key='parameters'):
+  with col1:
+      res = stl.text_input(label='Max acceptable resolution for templates')
+      if res == "":
+        stl.error("Please enter a valid input")
+      elif type(res) == str:
+        try:
+          res = float(res)
+        except:
+          stl.error("Please enter a valid input")
+      seqid = stl.text_input(label='Sequence identity threshold')
+      hmm_2_import = stl.text_input(label='HMM to import')
+      pdb_2_use = stl.text_input(label='MAX number of templates to download')
+  with col2:
+      eva = stl.text_input(label='E val')
+      number_of_wt_models = stl.text_input(label='Number of WT model to generate')
+      number_of_mut_models = stl.text_input(label='Number of MUTANTS model to generate')
+  stl.form_submit_button()
+  # print(type(res))
 
-    stl.form_submit_button()
 
 if input_file is not None:
-    save_file(input_file)
+  save_file(input_file)
 #    start_desktop()
-
-#if parameter is not None:
-#    print(eva)
