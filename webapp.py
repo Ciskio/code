d4cd4a33b74926d7226bae4bc12b845ce5b2505a
Francesco <50875832+Ciskio@users.noreply.github.com>
2021-11-18 16:37:09 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-import streamlit as stl
+mport streamlit as stl
 import os
 import subprocess
 
@@ -10,6 +10,23 @@ def save_file(infile):
 def start_desktop():
     cmdmod = "/opt/TurboVNC/bin/vncserver &" # starts the vncdesktop
     test = subprocess.run(cmdmod, shell=True, universal_newlines=True, check=True)
+
+def check_mistakes(parameter):
+  float_parameters = ["res", "seqid", "eva"]
+  if parameter == "":
+    stl.error("Please enter a valid input")
+  elif type(parameter) == str and parameter in float_parameters:
+    try:
+      parameter = float(parameter)
+    except:
+      stl.error("Please enter a valid input")
+
+  elif type(parameter) == str and parameter not in float_parameters:
+    try:
+      parameter = int(parameter)
+    except:
+      stl.error("Please enter a valid input")
+
 
 # title
 
@@ -23,20 +40,20 @@ col1, col2 = stl.columns(2) # make 2 columns for input parameters
 with stl.form(key='parameters'):
   with col1:
       res = stl.text_input(label='Max acceptable resolution for templates')
-      if res == "":
-        stl.error("Please enter a valid input")
-      elif type(res) == str:
-        try:
-          res = float(res)
-        except:
-          stl.error("Please enter a valid input")
+      check_mistakes(res)
       seqid = stl.text_input(label='Sequence identity threshold')
+      check_mistakes(seqid)
       hmm_2_import = stl.text_input(label='HMM to import')
+      check_mistakes(hmm_2_import)
       pdb_2_use = stl.text_input(label='MAX number of templates to download')
+      check_mistakes(pdb_2_use)
   with col2:
       eva = stl.text_input(label='E val')
+      check_mistakes(eva)
       number_of_wt_models = stl.text_input(label='Number of WT model to generate')
+      check_mistakes(number_of_wt_models)
       number_of_mut_models = stl.text_input(label='Number of MUTANTS model to generate')
+      check_mistakes(number_of_mut_models)
   stl.form_submit_button()
   # print(type(res))
 
