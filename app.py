2e1527074af3ef7124154e1e6157fad46cc31ce7
Francesco <50875832+Ciskio@users.noreply.github.com>
2021-11-20 17:57:14 +0100

    Update and rename mainpage.py to app.py

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import streamlit as st
 
 # Custom imports 
 from multipage import MultiPage
-from pages import automodeller, download_mutations # import your pages here
+import automodeller# import your pages here
 
 # Create an instance of the app 
 app = MultiPage()
 
 # Title of the main page
-st.title("Data Storyteller Application")
+st.title("Main Page")
 
 # Add all your applications (pages) here
-app.add_page("Upload Data", automodeller.py)
-app.add_page("Change Metadata", automodeller.py)
+app.add_page("Automodeller", automodeller.app)
+app.add_page("Download single point mutations", automodeller.app)
 # The main app
 app.run()
