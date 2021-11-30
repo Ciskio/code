118487f123e7cfd377a9f2150689e5e70596deef
Francesco <50875832+Ciskio@users.noreply.github.com>
2021-11-30 18:16:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ st.title("Main Page")
 
 # Add all your applications (pages) here
 app.add_page("Automodeller", automodeller.app)
-app.add_page("Download single point mutations", single_point.app)
+app.add_page("Download single point mutations", single_point_mutations.app)
 # The main app
 app.run()
