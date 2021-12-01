a93e552fe41a6d1f82366251b3e698064cb7467f
Francesco <50875832+Ciskio@users.noreply.github.com>
2021-12-01 10:10:38 +0100

    Update and rename connect_cheetah to connect_cheetah.sh

--- connect_cheetah.sh
+++ connect_cheetah.sh
@@ -1,6 +1,6 @@
 USERNAME=foliva
 PASSWD=`cat cheetah.txt | openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt -pass pass:SecretpASSword`
 
-REMOTE=altair.dsf.unica.it
+REMOTE=cheetah
 
 sshpass -p $PASSWD ssh -o StrictHostKeyChecking=no $USERNAME@$REMOTE
