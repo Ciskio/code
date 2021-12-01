3e367a2a706d5a70e350451d51c4446666a4474c
Francesco <50875832+Ciskio@users.noreply.github.com>
2021-12-01 10:31:42 +0100

    Update connect_altair.sh

--- connect_altair.sh
+++ connect_altair.sh
@@ -2,6 +2,6 @@
 USERNAME=foliva
 PASSWD=`cat altair.txt | openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt -pass pass:SecretpASSword`
 
-REMOTE=altair.dsf.unica.it
+REMOTE=192.84.153.220
 
 sshpass -p $PASSWD ssh -o StrictHostKeyChecking=no $USERNAME@$REMOTE
