863cfbddf0c9aa383c9619142f7220b3789cfd38
Francesco <50875832+Ciskio@users.noreply.github.com>
2021-12-02 14:47:35 +0100

    Delete connect_cheetah.sh

--- connect_cheetah.sh
+++
@@ -1,6 +0,0 @@
-USERNAME=foliva
-PASSWD=`cat cheetah.txt | openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt -pass pass:SecretpASSword`
-
-REMOTE=cheetah
-
-sshpass -p $PASSWD ssh -o StrictHostKeyChecking=no $USERNAME@$REMOTE
