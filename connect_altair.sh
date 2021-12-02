709bce90309e4851f40cc672e0d915f82f83db6b
Francesco <50875832+Ciskio@users.noreply.github.com>
2021-12-02 14:47:28 +0100

    Delete connect_altair.sh

--- connect_altair.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-USERNAME=foliva
-PASSWD=`cat altair.txt | openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt -pass pass:SecretpASSword`
-
-REMOTE=192.84.153.220
-
-sshpass -p $PASSWD ssh -o StrictHostKeyChecking=no $USERNAME@$REMOTE
